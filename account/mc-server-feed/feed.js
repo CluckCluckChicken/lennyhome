5069a5324aedc14d587c8cb9a232f27eac5d5b07
CluckCluckChicken <46281840+CluckCluckChicken@users.noreply.github.com>
2019-12-10 12:53:09 +0000

    Update feed.js

--- account/mc-server-feed/feed.js
+++ account/mc-server-feed/feed.js
@@ -1,3 +1,26 @@
 function CreateFeed(name) {
-  console.log("Attempting to create new feed...");
+  console.log("Attempting test whether this name already exists...");
+  
+  var newServerDocRef = db.collection("users").doc(uid).collection("mc-server-feed").doc(name);
+
+  feedBaseDocumentRef.get().then(function(doc) {
+      if (doc.exists) {
+        console.log("This feed already exists.");
+      } else {
+          // doc.data() will be undefined in this case
+          console.log("Name is valid. Creating feed entries...");
+          db.collection("users").doc(uid).collection("mc-server-feed").doc(name).set({
+            isServer: true,
+          })
+          .then(function() {
+              console.log("Done!");
+          })
+          .catch(function(error) {
+              console.error("Error writing new feed document: ", error);
+          });
+      }
+  }).catch(function(error) {
+      console.log("Error getting mc-server-feed base document:", error);
+      console.log("Hit F5 to retry.");
+  });
 }
