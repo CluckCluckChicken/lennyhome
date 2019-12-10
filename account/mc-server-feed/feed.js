2f21ae30d4e4f83bdce9c00536fe3af4aed33668
CluckCluckChicken <46281840+CluckCluckChicken@users.noreply.github.com>
2019-12-10 19:45:15 +0000

    Update feed.js

--- account/mc-server-feed/feed.js
+++ account/mc-server-feed/feed.js
@@ -76,42 +76,43 @@ if (user) {
 function CreateFeed(name) {
   
   if (user) {
-  var user = firebase.auth().currentUser;
-  var name, email, photoUrl, uid;
+    var user = firebase.auth().currentUser;
+    var name, email, photoUrl, uid;
 
-  if (user != null) {
-    name = user.displayName;
-    email = user.email;
-    photoUrl = user.photoURL;
-    uid = user.uid;  // The user's ID, unique to the Firebase project. Do NOT use
-                     // this value to authenticate with your backend server, if
-                     // you have one. Use User.getToken() instead.
+    if (user != null) {
+      name = user.displayName;
+      email = user.email;
+      photoUrl = user.photoURL;
+      uid = user.uid;  // The user's ID, unique to the Firebase project. Do NOT use
+                       // this value to authenticate with your backend server, if
+                       // you have one. Use User.getToken() instead.
+    }
+    
+      console.log("Attempting test whether this name already exists...");
+  
+      var newServerDocRef = db.collection("users").doc(uid).collection("mc-server-feed").doc(name);
+
+      feedBaseDocumentRef.get().then(function(doc) {
+          if (doc.exists) {
+            console.log("This feed already exists.");
+          } else {
+              // doc.data() will be undefined in this case
+              console.log("Name is valid. Creating feed entries...");
+              db.collection("users").doc(uid).collection("mc-server-feed").doc(name).set({
+                isServer: true,
+              })
+              .then(function() {
+                  console.log("Done!");
+                  console.log("Hit F5 to see it!");
+                  alert("Hit F5 to see your new server!");
+              })
+              .catch(function(error) {
+                  console.error("Error writing new feed document: ", error);
+              });
+          }
+      }).catch(function(error) {
+          console.log("Error getting mc-server-feed base document:", error);
+          console.log("Hit F5 to retry.");
+      });
   }
-  
-  console.log("Attempting test whether this name already exists...");
-  
-  var newServerDocRef = db.collection("users").doc(uid).collection("mc-server-feed").doc(name);
-
-  feedBaseDocumentRef.get().then(function(doc) {
-      if (doc.exists) {
-        console.log("This feed already exists.");
-      } else {
-          // doc.data() will be undefined in this case
-          console.log("Name is valid. Creating feed entries...");
-          db.collection("users").doc(uid).collection("mc-server-feed").doc(name).set({
-            isServer: true,
-          })
-          .then(function() {
-              console.log("Done!");
-              console.log("Hit F5 to see it!");
-              alert("Hit F5 to see your new server!");
-          })
-          .catch(function(error) {
-              console.error("Error writing new feed document: ", error);
-          });
-      }
-  }).catch(function(error) {
-      console.log("Error getting mc-server-feed base document:", error);
-      console.log("Hit F5 to retry.");
-  });
 }
