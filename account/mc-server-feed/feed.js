1108bfd22ee4a37a9c3e568268a1cb5085971b71
CluckCluckChicken <46281840+CluckCluckChicken@users.noreply.github.com>
2019-12-10 19:37:42 +0000

    Update feed.js

--- account/mc-server-feed/feed.js
+++ account/mc-server-feed/feed.js
@@ -27,6 +27,52 @@ if (user) {
                      // you have one. Use User.getToken() instead.
   }
 
+  // Change username options
+
+  console.log("You are logged in as: " + name + ". To change your username, type the following into the console:");
+  console.log("var user = firebase.auth().currentUser; user.updateProfile({ displayName: '[PUT YOUR NEW USERNAME HERE. NO SLASHES, OR QUOTATION MARKS OF ANY KIND. IS THAT CLEAR? YES SIR!]', })");
+  console.log("Ignore him. He's grumpy.");
+
+  var feedBaseDocumentRef = db.collection("users").doc(uid).collection("mc-server-feed").doc("%base");
+  var mcServerFeedRef = db.collection("/users/" + uid + "/mc-server-feed");
+
+  feedBaseDocumentRef.get().then(function(doc) {
+      if (doc.exists) {
+
+          // Get list of servers
+
+          console.log("Fetching servers list...");
+
+          // Create query to only show servers
+          var isServerQuery = mcServerFeedRef.where("isServer", "==", true);
+
+          isServerQuery.get()
+          .then(function(querySnapshot) {
+              querySnapshot.forEach(function(doc) {
+                  // doc.data() is never undefined for query doc snapshots
+                  console.log("You have the following servers:");
+                  console.log(doc.id);
+                  document.getElementById("serversList").innerHTML += "<button class='block w3-btn w3-dark-grey w3-round-large'>" + doc.id + "</button><input class='w3-input inline-block w3-round-large' id='newServerInput' style='width: 25%;' type='text'><button class='w3-btn w3-dark-grey w3-round-large inline-block' onclick='CreateFeed(document.getElementById(&#34newServerInput&#34).value)'>New server</button>";
+              });
+          })
+          .catch(function(error) {
+              console.log("Error fetching servers: ", error);
+              console.log("Hit F5 to retry.");
+          });
+
+      } else {
+        window.location = "./init.html";
+      }
+  }).catch(function(error) {
+      console.log("Error getting mc-server-feed base document:", error);
+      console.log("Hit F5 to retry.");
+  });
+
+} else {
+  window.location = "../index.html";
+}
+});
+
 function CreateFeed(name) {
   console.log("Attempting test whether this name already exists...");
   
