0213e6b5c26f3d2d09e51a5834050a6ef35c6b66
CluckCluckChicken <46281840+CluckCluckChicken@users.noreply.github.com>
2019-12-10 19:34:34 +0000

    Update feed.js

--- account/mc-server-feed/feed.js
+++ account/mc-server-feed/feed.js
@@ -13,6 +13,20 @@ firebase.initializeApp(firebaseConfig);
 
 var db = firebase.firestore();
 
+firebase.auth().onAuthStateChanged(function(user) {
+if (user) {
+  var user = firebase.auth().currentUser;
+  var name, email, photoUrl, uid;
+
+  if (user != null) {
+    name = user.displayName;
+    email = user.email;
+    photoUrl = user.photoURL;
+    uid = user.uid;  // The user's ID, unique to the Firebase project. Do NOT use
+                     // this value to authenticate with your backend server, if
+                     // you have one. Use User.getToken() instead.
+  }
+
 function CreateFeed(name) {
   console.log("Attempting test whether this name already exists...");
   
