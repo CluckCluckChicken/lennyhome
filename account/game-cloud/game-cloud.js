eae9da1ba4beba5b87bc5aef8c6a29cc23e09d65
CluckCluckChicken <46281840+CluckCluckChicken@users.noreply.github.com>
2019-12-12 16:12:40 +0000

    Create game-cloud.js

---
+++ account/game-cloud/game-cloud.js
@@ -0,0 +1,39 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAN4nLKenddvEzcSZYhaJ-s19Irv9PEdyA",
+  authDomain: "telepathic-squid-000.firebaseapp.com",
+  databaseURL: "https://telepathic-squid-000.firebaseio.com",
+  projectId: "telepathic-squid-000",
+  storageBucket: "telepathic-squid-000.appspot.com",
+  messagingSenderId: "216289155289",
+  appId: "1:216289155289:web:c4d915b3c17c08a5"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+
+firebase.auth().onAuthStateChanged(function(user) {
+if (user) {
+
+  var user = firebase.auth().currentUser;
+  var name, email, photoUrl, uid;
+  if (user != null) {
+    name = user.displayName;
+    email = user.email;
+    photoUrl = user.photoURL;
+    uid = user.uid;  // The user's ID, unique to the Firebase project. Do NOT use
+                     // this value to authenticate with your backend server, if
+                     // you have one. Use User.getToken() instead.
+  }
+
+  // Change username options
+
+  console.log("You are logged in as: " + name + ". To change your username, type the following into the console:");
+  console.log("var user = firebase.auth().currentUser; user.updateProfile({ displayName: '[PUT YOUR NEW USERNAME HERE. NO SLASHES, OR QUOTATION MARKS OF ANY KIND. IS THAT CLEAR? YES SIR!]', })");
+  console.log("Ignore him. He's grumpy.");
+
+} else {
+  window.location = "../index.html";
+}
+});
