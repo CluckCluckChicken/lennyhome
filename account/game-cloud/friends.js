250dacb1e588e77b45891740e74225489189f6ce
CluckCluckChicken <46281840+CluckCluckChicken@users.noreply.github.com>
2019-12-12 17:50:56 +0000

    Update friends.js

--- account/game-cloud/friends.js
+++ account/game-cloud/friends.js
@@ -22,7 +22,7 @@ if (user) {
     name = user.displayName;
     email = user.email;
     photoUrl = user.photoURL;
-    uid = user.uid;  // The user's ID, unique to the Firebase project. Do NOT use
+    window.uid = user.uid;  // The user's ID, unique to the Firebase project. Do NOT use
                      // this value to authenticate with your backend server, if
                      // you have one. Use User.getToken() instead.
   }
