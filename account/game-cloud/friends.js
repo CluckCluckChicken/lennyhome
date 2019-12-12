4256db59f7bde65274374aa85115d130ca88827d
CluckCluckChicken <46281840+CluckCluckChicken@users.noreply.github.com>
2019-12-12 18:22:54 +0000

    Update friends.js

--- account/game-cloud/friends.js
+++ account/game-cloud/friends.js
@@ -68,4 +68,6 @@ if (user) {
 function AddFriend(userToAdd) {
   var ref = firebase.database().ref('/users/' + window.uid + "/game-cloud/friends")
   ref.push(userToAdd);
+  console.log("Attempted to add new friend. Will reload.");
+  window.location.reload(true);
 }
