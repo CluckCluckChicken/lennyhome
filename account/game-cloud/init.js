12f3ca243f0002720cf365cf9ad494732d7b22b6
CluckCluckChicken <46281840+CluckCluckChicken@users.noreply.github.com>
2019-12-14 08:48:13 +0000

    Update init.js

--- account/game-cloud/init.js
+++ account/game-cloud/init.js
@@ -41,7 +41,8 @@ if (user) {
     else {
       console.log("Game Cloud isn't initialized, so it will be now.");
       firebase.database().ref('users/' + uid + "/game-cloud").set({
-        notifications: {welcomeNotification: {from: "system", content: "your account woz created"}}
+        notifications: {welcomeNotification: {from: "system", content: "your account woz created"}},
+        profile: {username: uid}
       }, function(error) {
           if (error) {
             console.log("Error: " + error + ". Retrying...");
